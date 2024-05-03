2cc0f94f76ee4598cf809b1e4a716a938f085bf6
Caitlin Teague Doerr <design@caitlinteague.com>
2024-05-02 20:02:10 -0500

    add about page and link on home for e2e tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import Login from '@/components/Login';
 
@@ -10,7 +11,10 @@ export default function Home() {
         width={80}
         alt="Legacy of Maela"
       />
-      <h1 className="mb-16 font-heading text-5xl">The Road to Teloceja</h1>
+      <h1 className="mb-2 font-heading text-5xl">The Road to Teloceja</h1>
+      <Link href="/about" className="mb-16 font-semibold text-accent ">
+        About
+      </Link>
       <main className="flex w-full justify-center">
         <Login title="Login to your account" />
       </main>
