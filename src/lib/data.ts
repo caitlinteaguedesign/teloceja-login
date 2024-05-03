798c25c4709c5f9d6040f66be5e51dc4ab702483
Caitlin Teague Doerr <design@caitlinteague.com>
2024-05-02 22:37:27 -0500

    correct entry

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 export const userData = [
   {
     name: 'Caitlin Teague Doerr',
-    email: 'design@caitlintague.com',
+    email: 'design@caitlinteague.com',
     password: 'admin',
   },
   { name: 'J.P. Miller', email: 'milz@yahoo.com', password: 'bubbers' },
