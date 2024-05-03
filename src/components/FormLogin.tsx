ecacf8fff3ce8ee56b0a8da3c0038b809a683603
Caitlin Teague Doerr <design@caitlinteague.com>
2024-05-03 00:07:53 -0500

    remove error message components

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -1,5 +1,4 @@
 import { signIn } from '@/lib/auth';
-import Image from 'next/image';
 
 export default function FormLogin() {
   return (
@@ -10,23 +9,6 @@ export default function FormLogin() {
         await signIn('credentials', formData);
       }}
     >
-      <div className="grid-cols-info-card grid items-center gap-x-2 gap-y-1 rounded-md border border-danger-400 bg-danger-100 p-3 pb-4">
-        <Image src="/warning-icon.svg" alt="warning" width={20} height={20} />
-        <h3 className="text-lg font-semibold text-parchment-700">
-          Incorrect email or password
-        </h3>
-        <p className="col-start-2">
-          The email or password you entered does not match our records. Please
-          try again, and contact{' '}
-          <a
-            href="mailto:support@maela.com"
-            className="font-semibold text-accent"
-          >
-            support@maela.com
-          </a>{' '}
-          if you are unable to access your account.
-        </p>
-      </div>
       <div className="grid gap-1">
         <label className="block">Email</label>
         <input
